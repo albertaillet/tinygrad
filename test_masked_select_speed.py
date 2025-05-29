ba8d9a80b0ff98b939472fce117599ab74ebde30
Albert Sund Aillet <albert@sundaillet.com>
2025-05-29 12:48:10 +0200

    changed test from 2d to 1d

--- test_masked_select_speed.py
+++ test_masked_select_speed.py
@@ -2,23 +2,21 @@ import time
 from tinygrad import dtypes, Tensor
 from tinygrad.helpers import getenv
 
-def make_square_mask(shape, mask_size:int) -> Tensor:
-  BS, _, H, W = shape
-  low_x = Tensor.randint(BS, low=0, high=W-mask_size).reshape(BS,1,1,1)
-  low_y = Tensor.randint(BS, low=0, high=H-mask_size).reshape(BS,1,1,1)
-  idx_x = Tensor.arange(W, dtype=dtypes.int32).reshape((1,1,1,W))
-  idx_y = Tensor.arange(H, dtype=dtypes.int32).reshape((1,1,H,1))
-  return (idx_x >= low_x) * (idx_x < (low_x + mask_size)) * (idx_y >= low_y) * (idx_y < (low_y + mask_size))
+def make_mask(shape, mask_size:int) -> Tensor:
+  BS, _, W = shape
+  low_x = Tensor.randint(BS, low=0, high=W-mask_size).reshape(BS,1,1)
+  idx_x = Tensor.arange(W, dtype=dtypes.int32).reshape((1,1,W))
+  return (idx_x >= low_x) * (idx_x < (low_x + mask_size))
 
 def random_crop(X:Tensor, crop_size:int):
-  mask = make_square_mask(X.shape, crop_size)
-  mask = mask.expand((-1,3,-1,-1))
+  mask = make_mask(X.shape, crop_size)
+  mask = mask.expand((-1,3,-1))
   X_cropped = Tensor(X.numpy()[mask.numpy()])
-  return X_cropped.reshape((-1, 3, crop_size, crop_size))
+  return X_cropped.reshape((-1, 3, crop_size))
 
 def random_crop_in_tinygrad(X:Tensor, crop_size:int):
-  mask = make_square_mask(X.shape, crop_size)
-  return X.masked_select(mask).reshape((-1, 3, crop_size, crop_size))
+  mask = make_mask(X.shape, crop_size)
+  return X.masked_select(mask).reshape((-1, 3, crop_size))
 
 def test_crop(X:Tensor, crop_size:int, seed:int):
   t1 = time.monotonic()
@@ -35,7 +33,7 @@ def test_crop(X:Tensor, crop_size:int, seed:int):
 if __name__ == "__main__":
   BS, SEED = getenv("BS", 512), getenv("SEED", 42)
   Tensor.manual_seed(SEED)
-  X = Tensor.rand((BS, 3, 32, 32), dtype=dtypes.float32)
+  X = Tensor.rand((BS, 3, 32), dtype=dtypes.float32)
   print(f"Batch size: {BS}, Seed: {SEED}")
   test_crop(X, crop_size=32, seed=SEED)
   print("Tests passed")
