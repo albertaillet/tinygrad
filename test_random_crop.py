e5d9962b2a054f5f498058ecffe7186cee99cd80
Albert Sund Aillet <albert@sundaillet.com>
2025-06-02 19:48:26 +0200

    remove random_crop_padded_index

--- test_random_crop.py
+++ test_random_crop.py
@@ -29,22 +29,7 @@ def random_crop_masked_select(X:Tensor, crop_size:int):
   mask = make_square_mask(X.shape, crop_size)
   return X.masked_select(mask).reshape((-1, 3, crop_size, crop_size))
 
-def crop_indices(batch_size:int, dim_size:int, pad_size:int):
-  low, idx = random_low_and_indices(batch_size, dim_size, 2*pad_size)
-  idx = (idx - pad_size + low)  # start from padding and add offset
-  idx = idx.abs() # left reflect
-  idx = (idx < dim_size).where(idx, 2*(dim_size-1)-idx) # right reflect
-  return idx
-
 def random_crop_index(X:Tensor, pad_size:int):
-  BS, C, H, W = X.shape
-  idx_x = crop_indices(BS, W, pad_size).reshape(BS, 1, 1, W)
-  idx_y = crop_indices(BS, H, pad_size).reshape(BS, 1, H, 1)
-  idx_flat = (idx_y * W + idx_x).reshape(BS, 1, H*W).expand(BS, C, H*W)
-  X_flat = X.reshape(BS, C, H*W)
-  return X_flat.gather(2, idx_flat).reshape(BS, C, H, W)
-
-def random_crop_padded_index(X:Tensor, pad_size:int):
   BS, C, H_pad, W_pad = X.shape
   H, W = H_pad - 2*pad_size, W_pad - 2*pad_size
   low_x, idx_x = random_low_and_indices(BS, W, 2*pad_size)
@@ -64,28 +49,24 @@ def test_crop(X:Tensor, crop_size:int, seed:int, pad_size:int):
   X_padded = pad_reflect(X, size=pad_size)
   t1 = time.monotonic()
   set_seed(seed)
-  X_cropped = random_crop(X_padded, crop_size=crop_size).numpy()
+  X_cropped_numpy = random_crop(X_padded, crop_size=crop_size).numpy()
   t2 = time.monotonic()
   set_seed(seed)
-  if X.shape[0] < 5000:
-    X_cropped_in_tinygrad = random_crop_masked_select(X_padded, crop_size=crop_size).numpy()
+  X_cropped_index = random_crop_index(X_padded, pad_size=pad_size).numpy()
   t3 = time.monotonic()
   set_seed(seed)
-  X_cropped_index = random_crop_index(X, pad_size=pad_size).numpy()
+  if X_padded.shape[0] < 5000:
+    X_cropped_masked_select = random_crop_masked_select(X_padded, crop_size=crop_size).numpy()
   t4 = time.monotonic()
-  set_seed(seed)
-  X_cropped_padded_index = random_crop_padded_index(X_padded, pad_size=pad_size).numpy()
-  t5 = time.monotonic()
   for k,v in locals().items():
     if k.startswith("X") and PRINT: print(f"{k:20}\n{v.numpy() if isinstance(v, Tensor) else v}\n")
-  assert (X_cropped == X_cropped_index).all(), "Cropped results with index do not match"
-  assert (X_cropped == X_cropped_padded_index).all(), "Cropped results with padded index do not match"
+  assert (X_cropped_numpy == X_cropped_index).all(), "Cropped results with index do not match"
   if X.shape[0] < 5000:
-    assert (X_cropped == X_cropped_in_tinygrad).all(), "Cropped results do not match"
-  print(f"Dataset shape: {str(X.shape):>18}, {(t2-t1)*1000.0:7.2f} ms numpy, {(t5-t4)*1000.0:7.2f} ms index, {(t3-t2)*1000.0:7.2f} ms masked_select, {(t4-t3)*1000.0:7.2f} ms unpadded index")
+    assert (X_cropped_numpy == X_cropped_masked_select).all(), "Cropped results do not match"
+  print(f"Dataset shape: {str(X_padded.shape):>18}, {(t2-t1)*1000.0:7.2f} ms numpy, {(t3-t2)*1000.0:7.2f} ms index, {(t4-t3)*1000.0:7.2f} ms masked_select")
 
 if __name__ == "__main__":
-  BS, SIZE, PAD_SIZE = getenv("BS", 50000), getenv("SIZE", 32), getenv("PAD_SIZE", 2)
+  SIZE, PAD_SIZE = getenv("SIZE", 32), getenv("PAD_SIZE", 2)
   SEED, CIFAR, PRINT = getenv("SEED", 42), getenv("CIFAR", 1), getenv("PRINT", 0)
   for BS in [100, 500, 50000]:
     if CIFAR:
